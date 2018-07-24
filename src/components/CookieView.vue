bdaa01dc195c311f1b81689c7f46b5eb8f1435ef
Alexandre Payen <alexandre@Alexandres-MacBook-Pro-3.local>
2018-07-24 11:10:02 +0200

    intial commit

---
+++ src/components/CookieView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="cookie-view">
+        <div class="cookie-numbers">
+            <h2>{{counter}} Cookies</h2>
+            <p>per seconds : 0</p>
+        </div>
+        <button @click="increment">click me</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CookieView",
+    }
+</script>
+
+<style scoped>
+    .cookie-view{
+        width: 30vw;
+    }
+</style>
